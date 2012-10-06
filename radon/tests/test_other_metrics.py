a8c564eba480341f63f70718ed001f4032f99ddc
Michele Lacchia <michelelacchia@gmail.com>
2012-10-06 20:34:19 +0200

    Add a failing test for h_visit

--- radon/tests/test_other_metrics.py
+++ radon/tests/test_other_metrics.py
@@ -1,5 +1,9 @@
+import textwrap
 from paramunittest import *
 from radon.metrics import *
+
+
+dedent = lambda code: textwrap.dedent(code).strip()
 
 
 COMPUTE_MI_CASES = [
@@ -19,6 +23,35 @@ class TestComputeMI(ParametrizedTestCase):
 
     def testComputeMI(self):
         self.assertAlmostEqual(compute_mi(*self.values), self.expected)
+
+
+H_VISIT_CASES = [
+    ('''
+     ''', (0,) * 12),
+
+    ('''
+     a = b + c
+     d = c - f
+
+     def f(b):
+         a = 2 - 4
+         d = a + b
+         return a ** d
+     ''', (3, 7, 5, 10, 10, 15, 24.406371956566698, 49.82892142331044,
+           2.142857142857143, 106.77626019280807, 5.932014455156004,
+           0.016609640474436815)),
+]
+
+
+@parametrized(*H_VISIT_CASES)
+class TestHVisit(ParametrizedTestCase):
+
+    def setParameters(self, code, expected):
+        self.code = dedent(code)
+        self.expected = expected
+
+    def testHVisit(self):
+        self.assertEqual(h_visit(self.code), self.expected)
 
 
 if __name__ == '__main__':
