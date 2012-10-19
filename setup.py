98e2e2b899cf3cb720151c004e05b7f7a946e949
Michele Lacchia <michelelacchia@gmail.com>
2012-10-19 17:56:54 +0200

    Fix #12

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
+import os
 from setuptools import setup
 import radon
 
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fobj:
+    readme = fobj.read()
 
 setup(name='radon',
       version=radon.__version__,
       author='Michele Lacchia',
       author_email='michelelacchia@gmail.com',
       license='MIT',
+      description='',
+      long_description=readme,
       packages=['radon', 'radon.tests'],
       tests_require=['tox'],
       extras_require={'tool': ['baker']},
       entry_points={'console_scripts': ['radon = radon:main']}
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Quality Assurance',
+          'Topic :: Utilities',
+      ]
 )
