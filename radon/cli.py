aed237a0b166ab1050173c9ca832d5c23b356c7f
Michele Lacchia <michelelacchia@gmail.com>
2013-11-20 18:45:48 +0100

    Fix #33
    Add -j, --json option to the `cc` command. If present, the output will be
    regular json. Filtering with -n --min, -x --max works as expected.

--- radon/cli.py
+++ radon/cli.py
@@ -18,8 +18,9 @@ except ImportError:
 
 import os
 import sys
+import json as json_mod
 import radon.complexity as cc_mod
-from radon.tools import iter_filenames
+from radon.tools import iter_filenames, cc_to_dict, _filter_by_rank
 from radon.complexity import cc_visit, cc_rank, sorted_results
 from radon.raw import analyze
 from radon.metrics import mi_visit, mi_rank
@@ -47,9 +48,12 @@ def log(msg, *args, **kwargs):
 
     `indent`, if present as a keyword argument, specifies the indent level, so
     that `indent=0` will log normally, `indent=1` will indent the message by 4
-    spaces, &c..'''
+    spaces, &c..
+    `noformat`, if present and True, will cause the message not to be formatted
+    in any way.'''
     indent = 4 * kwargs.get('indent', 0)
-    sys.stdout.write(' ' * indent + msg.format(*args) + '\n')
+    m = msg if kwargs.get('noformat', False) else msg.format(*args)
+    sys.stdout.write(' ' * indent + m + '\n')
 
 
 def log_list(lst, **kwargs):
@@ -76,12 +80,10 @@ def _format_line(line, ranked, show_complexity=False):
                            compl, reset=RESET)
 
 
-def _print_cc_results(path, results, min, max, show_complexity):
+def _print_cc_results(path, results, show_complexity):
     '''Print Cyclomatic Complexity results.
 
     :param path: the path of the module that has been analyzed
-    :param min: the minimum complexity rank to show
-    :param max: the maximum complexity rank to show
     :param show_complexity: if True, show the complexity score in addition to
         the complexity rank
     '''
@@ -90,13 +92,23 @@ def _print_cc_results(path, results, min, max, show_complexity):
     for line in results:
         ranked = cc_rank(line.complexity)
         average_cc += line.complexity
-        if not min <= ranked <= max:
-            continue
         res.append(_format_line(line, ranked, show_complexity))
     if res:
         log(path)
         log_list(res, indent=1)
     return average_cc, len(results)
+
+
+def analyze_cc(paths, exclude, min, max, order_function):
+    for name in iter_filenames(paths, exclude):
+        with open(name) as fobj:
+            try:
+                results = sorted_results(cc_visit(fobj.read()), order_function)
+                yield name, list(_filter_by_rank(results, min, max))
+            except Exception as e:
+                log(name, indent=1)
+                log_error(e, indent=1)
+                continue
 
 
 @BAKER.command(shortopts={'multi': 'm', 'exclude': 'e', 'show': 's'})
@@ -130,9 +142,10 @@ def mi(multi=True, exclude=None, show=False, *paths):
 
 
 @BAKER.command(shortopts={'min': 'n', 'max': 'x', 'show_complexity': 's',
-                          'average': 'a', 'exclude': 'e', 'order': 'o'})
+                          'average': 'a', 'exclude': 'e', 'order': 'o',
+                          'json': 'j'})
 def cc(min='A', max='F', show_complexity=False, average=False,
-       exclude=None, order='SCORE', *paths):
+       exclude=None, order='SCORE', json=False, *paths):
     '''Analyze the given Python modules and compute Cyclomatic
     Complexity (CC).
 
@@ -147,6 +160,8 @@ def cc(min='A', max='F', show_complexity=False, average=False,
         together with the A-F rank. Default to False.
     -a, --average  If True, at the end of the analysis display the average
         complexity. Default to False.
+    -o, --order  The ordering function. Can be SCORE, LINES or ALPHA.
+    -j, --json  Format results in JSON.
     paths  The modules or packages to analyze.
     '''
     min = min.upper()
@@ -154,18 +169,17 @@ def cc(min='A', max='F', show_complexity=False, average=False,
     average_cc = .0
     analyzed = 0
     order_function = getattr(cc_mod, order.upper(), getattr(cc_mod, 'SCORE'))
-    for name in iter_filenames(paths, exclude):
-        with open(name) as fobj:
-            try:
-                results = sorted_results(cc_visit(fobj.read()), order_function)
-            except Exception as e:
-                log(name, indent=1)
-                log_error(e, indent=1)
-                continue
-        cc, blocks = _print_cc_results(name, results, min, max,
-                                       show_complexity)
-        average_cc += cc
-        analyzed += blocks
+    cc_data = analyze_cc(paths, exclude, min, max, order_function)
+    if json:
+        result = {}
+        for key, data in cc_data:
+            result[key] = map(cc_to_dict, data)
+        log(json_mod.dumps(result), noformat=True)
+    else:
+        for name, results in cc_data:
+            cc, blocks = _print_cc_results(name, results, show_complexity)
+            average_cc += cc
+            analyzed += blocks
 
     if average and analyzed:
         cc = average_cc / analyzed
--- radon/tools.py
+++ radon/tools.py
@@ -3,6 +3,8 @@ import operator
 import itertools
 from functools import reduce
 from pathfinder import find_paths, FnmatchFilter, NotFilter, FileFilter
+from radon.visitors import Function
+from radon.complexity import cc_rank
 
 
 def iter_filenames(paths, exclude=None):
@@ -24,3 +26,30 @@ def build_filter(exclude=None):
                 reduce(operator.or_, excluded[1:], excluded[0])
              )
     return f
+
+
+def cc_to_dict(obj):
+    def get_type(obj):
+        if isinstance(obj, Function):
+            return 'method' if obj.is_method else 'function'
+        return 'class'
+
+    result = {}
+    result['type'] = get_type(obj)
+    #TODO: Avoid hard-coding attributes
+    attrs = ['name', 'lineno', 'col_offset', 'endline', 'classname',
+             'complexity', 'real_complexity']
+    for a in attrs:
+        v = getattr(obj, a, None)
+        if v is not None:
+            result[a] = v
+    for key in ('methods', 'clojures'):
+        if hasattr(obj, key):
+            result[key] = map(cc_to_dict, getattr(obj, key))
+    return result
+
+
+def _filter_by_rank(results, min, max):
+    for result in results:
+        if min <= cc_rank(result.complexity) <= max:
+            yield result
