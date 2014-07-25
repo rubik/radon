70b93382530601a0888197d5da255a793bbba61d
Michele Lacchia <michelelacchia@gmail.com>
2014-07-25 21:37:54 +0200

    Show help if no command is given

--- radon/__init__.py
+++ radon/__init__.py
@@ -3,8 +3,11 @@ __version__ = '0.5.2'
 
 def main():
     '''The entry point for Setuptools.'''
+    import sys
     from radon.cli import program, log_error
 
+    if not sys.argv[1:]:
+        sys.argv.append('-h')
     try:
         program()
     except Exception as e:
