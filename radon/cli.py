94e1174171d2a2b74667b0843fbe8bc375897e75
Michele Lacchia <michelelacchia@gmail.com>
2014-08-01 19:41:51 +0200

    Bump to 0.5.3

--- radon/cli.py
+++ radon/cli.py
@@ -23,7 +23,7 @@ from radon.complexity import cc_visit, cc_rank, sorted_results
 from radon.raw import analyze
 from radon.metrics import mi_visit, mi_rank
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 
 RANKS_COLORS = {'A': GREEN, 'B': GREEN,
