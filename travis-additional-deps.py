9aa15a0b2aedadb68b39db54c6d16b7140bbb9b1
Michele Lacchia <michelelacchia@gmail.com>
2015-07-14 08:41:31 +0200

    fix(deps): backport coverage on Travis for Python 3.2

--- travis-additional-deps.py
+++ travis-additional-deps.py
@@ -5,11 +5,15 @@ import sys
 import subprocess
 
 deps = []
+undeps = []
 pyv = os.environ.get('TRAVIS_PYTHON_VERSION', '%s.%s' % sys.version_info[:2])
 if pyv == '2.6':
     deps.extend(['unittest2', 'argparse', 'importlib', 'mock'])
-elif pyv in ('2.7', '3.2', 'pypy'):
-    deps.append('mock')
+elif pyv == '3.2':
+    undeps.append('coverage')
+    deps.append('coverage==3.7.1')
 
+if undeps:
+    subprocess.call(['pip', 'uninstall', '-y'] + undeps)
 if deps:
     subprocess.call(['pip', 'install'] + deps)
