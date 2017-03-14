a34f0b6041096ca4c8089e550e55607942f8a53d
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2017-03-14 02:57:19 -0700

    fix(raw): use python tokenizer to identify single-line comments and docstrings. (#106)
    * Use python tokenizer to identify single-line comments and docstrings.
    
    Perform the raw analysis using a single iterator.
    Add test cases that cover line continuations and unusual docstrings.
    
    * Require py.test 2.7 or more.

--- radon/raw.py
+++ radon/raw.py
@@ -28,7 +28,9 @@ TOKEN_NUMBER = operator.itemgetter(0)
 #   loc = Lines of Code (total lines)
 #   lloc = Logical Lines of Code
 #   comments = Comments lines
+#   multi = Multi-line strings (assumed to be docstrings)
 #   blank = Blank lines (or whitespace-only lines)
+#   single_comments = Single-line comments or docstrings
 Module = collections.namedtuple('Module', ['loc', 'lloc', 'sloc',
                                            'comments', 'multi', 'blank',
                                            'single_comments'])
@@ -80,25 +82,24 @@ def _get_all_tokens(line, lines):
     shortest tokenization possible. This is done by catching
     :exc:`tokenize.TokenError` when a multi-line string or statement is
     encountered.
+    :returns: tokens, lines
     '''
-    sloc_increment = multi_increment = 0
-    try:
-        tokens = _generate(line)
-    except tokenize.TokenError:
-        # A multi-line string or statement has been encountered:
-        # start adding lines and stop when tokenize stops complaining
-        while True:
-            sloc_increment += 1
-            line = '\n'.join([line, next(lines)])
-            try:
-                tokens = _generate(line)
-            except tokenize.TokenError:
-                continue
-            if tokens[0][0] == 3 and len(tokens) == 2:
-                # Multi-line string detected
-                multi_increment += line.count('\n') + 1
-            break
-    return tokens, sloc_increment, multi_increment
+    used_lines = [line]
+    while True:
+        try:
+            tokens = _generate(line)
+        except tokenize.TokenError:
+            # A multi-line string or statement has been encountered:
+            # start adding lines and stop when tokenize stops complaining
+            pass
+        else:
+            if not any(t[0] == tokenize.ERRORTOKEN for t in tokens):
+                return tokens, used_lines
+
+        # Add another line
+        next_line = next(lines)
+        line = line + '\n' + next_line
+        used_lines.append(next_line)
 
 
 def _logical(tokens):
@@ -149,147 +150,6 @@ def _logical(tokens):
     return sum(aux(sub) for sub in _split_tokens(tokens, OP, ';'))
 
 
-def remove_lines(doc, lines_to_remove):
-    '''Removes lines from a document.
-    :param doc: [str], document cast into an array.
-    :param lines_to_remove: [int], list of lines to remove from the doc.
-    :return: [str], doc with specified lines removed.
-    '''
-
-    for line_number in lines_to_remove:
-        doc[line_number] = []
-    return [line.strip() for line in doc if line]
-
-
-def is_multiline_string(doc, line_count, quote_type):
-    '''Cases to catch multiline_strings.
-    :param doc: [str], a document cast into an array.
-    :param line_count: int, zero based index that points to the current line
-                 in an docuement.
-    :param quote_type: str, one of the two multiline quotes available in
-                       Python.
-    :return: bool, True if the triple quoted line is a multiline string.
-    '''
-
-    line = doc[line_count]
-    previous_line = doc[line_count - 1]
-
-    if line.count('=') > 0 and line.index('=') < line.index(quote_type) \
-            or line_count != 0 and len(previous_line) > 2 \
-            and '=' in previous_line[-3]:
-        return True
-
-    return False
-
-
-def find_multiline_comments(lines_to_remove, end, doc, line_count, quote_type):
-    '''
-    :param lines_to_remove: [int], a zero based index that represents lines to
-                            to be removed from a document.
-    :param end: bool, if True then the first of the two multiline comments has
-                been found.
-    :param doc: [str], a document cast into an array.
-    :param line_count: int, zero based index that points to the current line
-                 in an docuement.
-    :param quote_type: str, one of the two multiline quotes available in
-                       Python.
-    :return: tuple, lines_to_remove = same as that passed in, with additions.
-                    end = bool, updated version of end paramater.
-    '''
-
-    # Exceptions: Quote type needs to exist, to get the first line of a
-    # multine comment it cannot be the last.
-    if quote_type and end is False and line_count < len(doc) - 1:
-        quote_type = quote_type[0]
-        end = True
-
-        if is_multiline_string(doc, line_count, quote_type):
-            # If a multiline string is found move the cursor until the end
-            # of the multiline string.
-            line_count += 1
-            while quote_type not in doc[line_count]:
-                line_count += 1
-                if quote_type in doc[line_count]:
-                    line_count += 1
-                    break
-            return lines_to_remove, False, line_count
-
-        lines_to_remove.append(line_count)
-        return lines_to_remove, end, line_count
-
-    elif end and not quote_type:
-        lines_to_remove.append(line_count)
-
-    elif end and quote_type:
-        lines_to_remove.append(line_count)
-        end = False
-
-    return lines_to_remove, end, line_count
-
-
-def find_comments(lines_to_remove, line_count, line):
-    '''Find single line comments in a python file.
-    :param lines_to_remove: [int], a zero based index that represents lines to
-                            to be removed from a document.
-    :param line_count: int, zero based index that points to the current line
-                       in an docuement.
-    :param line: str, the current line in a document being examined.
-    :return: [int], same as parameter with additional indices that were found.
-    '''
-
-    if not line:
-        return (lines_to_remove, True)
-
-    if line[0] == "#" or line.count("'''") == 2 and '=' not in line or\
-            line.count('"""') == 2 and '=' not in line:
-        lines_to_remove.append(line_count)
-        return (lines_to_remove, True)
-
-    return (lines_to_remove, False)
-
-
-def remove_python_documentation(doc):
-    '''Removes all the documentation from python code.
-    :param doc: [str], each line of a code recasted as an array
-    :return: [str], doc that was passed in, excluding lines of documentation.
-    '''
-
-    multi_quos = ["'''", '"""']
-    lines_to_remove = []
-    end = False
-    comments = 0
-    multi = 1
-    new_count = 0
-    for line_count, line in enumerate(doc):
-
-        if new_count > line_count:
-            continue
-        lines_to_remove, removed = find_comments(lines_to_remove,
-                                                 line_count,
-                                                 line)
-        if removed:
-            comments += 1
-            continue
-
-        quote_type = [multi_quo for multi_quo in multi_quos
-                      if multi_quo in doc[line_count]]
-
-        # end is True if the first of a pair of multiline comments is found and
-        # end will revert back to False when both pairs are found.
-        lines_to_remove, end, new_count = find_multiline_comments(
-            lines_to_remove=lines_to_remove,
-            end=end,
-            doc=doc,
-            line_count=line_count,
-            quote_type=quote_type)
-        if end:
-            multi += 1
-    # Set multi equal to 0 if no multi strings were found
-    if multi == 1:
-        multi = 0
-    return len(remove_lines(doc, lines_to_remove)), comments, multi
-
-
 def analyze(source):
     '''Analyze the source code and return a namedtuple with the following
     fields:
@@ -300,34 +160,56 @@ def analyze(source):
             corresponding to the LLOC)
         * **comments**: The number of Python comment lines
         * **multi**: The number of lines which represent multi-line strings
+        * **single_comments**: The number of lines which are just comments with no code
         * **blank**: The number of blank lines (or whitespace-only ones)
 
-    The equation :math:`sloc + blanks = loc` should always hold.
+    The equation :math:`sloc + blanks + multi + single_comments = loc` should always hold.
     Multiline strings are not counted as comments, since, to the Python
     interpreter, they are not comments but strings.
     '''
-    lloc = comments = multi = blank = 0
-
-    # Cast source code into an array, devoid of blank lines
-    source_array = [line.strip() for line in source.split('\n') if line]
-    sloc = len(source_array)
-    loc, single_comments, multi = remove_python_documentation(source_array)
-    lines = iter(source.splitlines())
+    lloc = comments = single_comments = multi = blank = sloc = 0
+    lines = (l.strip() for l in source.splitlines())
     for lineno, line in enumerate(lines, 1):
-        line = line.strip()
-        if not line:
-            blank += 1
-            continue
         try:
             # Process a logical line that spans on multiple lines
-            tokens, _, _ = _get_all_tokens(line, lines)
+            tokens, parsed_lines = _get_all_tokens(line, lines)
         except StopIteration:
             raise SyntaxError('SyntaxError at line: {0}'.format(lineno))
-        # Add the comments
-        comments += list(map(TOKEN_NUMBER, tokens)).count(COMMENT)
+
+        for parsed_line in parsed_lines:
+            if parsed_line:
+                sloc += 1
+            else:
+                blank += 1
+
+        comments += [TOKEN_NUMBER(t) for t in tokens].count(tokenize.COMMENT)
+
+        # Identify single line comments
+        if TOKEN_NUMBER(tokens[0]) == tokenize.COMMENT:
+            single_comments += 1
+
+        # Identify docstrings
+        if TOKEN_NUMBER(tokens[0]) == tokenize.STRING:
+            _, _, (start_row, start_col), _, _ = tokens[0]
+
+            # Find the end of any docstrings concatenated through adjacency
+            for t in tokens:
+                if TOKEN_NUMBER(t) == tokenize.STRING:
+                    _, _, _, (end_row, end_col), _ = t
+                else:
+                    break
+
+            if end_row == start_row:
+                # Consider single-line docstrings separately from other multiline docstrings
+                single_comments += 1
+            else:
+                multi += sum(1 for l in parsed_lines if l)  # Skip empty lines
+
         # Process a logical line
         # Split it on semicolons because they increase the number of logical
         # lines
         for sub_tokens in _split_tokens(tokens, OP, ';'):
             lloc += _logical(sub_tokens)
+
+    loc = sloc - multi - single_comments
     return Module(loc, lloc, sloc, comments, multi, blank, single_comments)
