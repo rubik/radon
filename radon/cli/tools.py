d76d7a2efa926b268cddf18384cc103943c26185
Michele Lacchia <michelelacchia@gmail.com>
2017-09-07 09:35:42 +0200

    style(tools): PEP8 fixes

--- radon/cli/tools.py
+++ radon/cli/tools.py
@@ -56,36 +56,36 @@ else:
         blank_re = re.compile(r'^[ \t\f]*(?:[#\r\n]|$)')
 
         def _get_normal_name(orig_enc):
-            """Imitates get_normal_name in tokenizer.c."""
+            '''Imitates get_normal_name in tokenizer.c.'''
             # Only care about the first 12 characters.
-            enc = orig_enc[:12].lower().replace("_", "-")
-            if enc == "utf-8" or enc.startswith("utf-8-"):
-                return "utf-8"
-            if enc in ("latin-1", "iso-8859-1", "iso-latin-1") or \
-               enc.startswith(("latin-1-", "iso-8859-1-", "iso-latin-1-")):
-                return "iso-8859-1"
+            enc = orig_enc[:12].lower().replace('_', '-')
+            if enc == 'utf-8' or enc.startswith('utf-8-'):
+                return 'utf-8'
+            if enc in ('latin-1', 'iso-8859-1', 'iso-latin-1') or \
+               enc.startswith(('latin-1-', 'iso-8859-1-', 'iso-latin-1-')):
+                return 'iso-8859-1'
             return orig_enc
 
-
         def detect_encoding(readline):
-            """
-            The detect_encoding() function is used to detect the encoding that should
-            be used to decode a Python source file. It requires one argument, readline,
-            in the same way as the tokenize() generator.
+            '''
+            The detect_encoding() function is used to detect the encoding that
+            should be used to decode a Python source file. It requires one
+            argument, readline, in the same way as the tokenize() generator.
 
-            It will call readline a maximum of twice, and return the encoding used
-            (as a string) and a list of any lines (left as bytes) it has read in.
+            It will call readline a maximum of twice, and return the encoding
+            used (as a string) and a list of any lines (left as bytes) it has
+            read in.
 
-            It detects the encoding from the presence of a utf-8 bom or an encoding
-            cookie as specified in pep-0263. If both a bom and a cookie are present,
-            but disagree, a SyntaxError will be raised. If the encoding cookie is an
-            invalid charset, raise a SyntaxError. Note that if a utf-8 bom is found,
-            'utf-8-sig' is returned.
+            It detects the encoding from the presence of a utf-8 bom or an
+            encoding cookie as specified in pep-0263. If both a bom and a
+            cookie are present, but disagree, a SyntaxError will be raised. If
+            the encoding cookie is an invalid charset, raise a SyntaxError.
+            Note that if a utf-8 bom is found, 'utf-8-sig' is returned.
 
-            If no encoding is specified, then the default of 'utf-8' will be returned.
-            The third argument indicates whether the encoding cookie was found
-            or not.
-            """
+            If no encoding is specified, then the default of 'utf-8' will be
+            returned.  The third argument indicates whether the encoding cookie
+            was found or not.
+            '''
             try:
                 filename = readline.__self__.name
             except AttributeError:
@@ -93,6 +93,7 @@ else:
             bom_found = False
             encoding = None
             default = 'utf-8'
+
             def read_or_stop():
                 try:
                     return readline()
@@ -101,12 +102,12 @@ else:
 
             def find_cookie(line):
                 try:
-                    # Decode as UTF-8. Either the line is an encoding declaration,
-                    # in which case it should be pure ASCII, or it must be UTF-8
-                    # per default encoding.
+                    # Decode as UTF-8. Either the line is an encoding
+                    # declaration, in which case it should be pure ASCII, or it
+                    # must be UTF-8 per default encoding.
                     line_string = line.decode('utf-8')
                 except UnicodeDecodeError:
-                    msg = "invalid or missing encoding declaration"
+                    msg = 'invalid or missing encoding declaration'
                     if filename is not None:
                         msg = '{} for {!r}'.format(msg, filename)
                     raise SyntaxError(msg)
@@ -116,14 +117,14 @@ else:
                     return None
                 encoding = _get_normal_name(match.group(1))
                 try:
-                    codec = lookup(encoding)
+                    lookup(encoding)
                 except LookupError:
                     # This behaviour mimics the Python interpreter
                     if filename is None:
-                        msg = "unknown encoding: " + encoding
+                        msg = 'unknown encoding: ' + encoding
                     else:
-                        msg = "unknown encoding for {!r}: {}".format(filename,
-                                encoding)
+                        msg = ('unknown encoding for {!r}: '
+                               '{}'.format(filename, encoding))
                     raise SyntaxError(msg)
 
                 if bom_found:
@@ -132,7 +133,8 @@ else:
                         if filename is None:
                             msg = 'encoding problem: utf-8'
                         else:
-                            msg = 'encoding problem for {!r}: utf-8'.format(filename)
+                            msg = ('encoding problem for '
+                                   '{!r}: utf-8'.format(filename))
                         raise SyntaxError(msg)
                     encoding += '-sig'
                 return encoding
@@ -161,11 +163,10 @@ else:
 
             return default, [first, second], False
 
-
         def _open_function(filename, encoding=None):
-            """Open a file in read only mode using the encoding detected by
+            '''Open a file in read only mode using the encoding detected by
             detect_encoding().
-            """
+            '''
             # Note: Python 3 uses builtins.open here..
             buffer = _io_open_function(filename, 'rb')
             try:
